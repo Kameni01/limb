aff922ddc31bdbcf93bf81fbe93faa41cf15a85b
Kameni01 <kameni01@mail.ru>
2019-01-10 06:40:18 +0500

    Подключил модуль "Редактор статей summernote"

--- limb/home/admin.py
+++ limb/home/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from home.models import News, Category, Tag
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
-admin.site.register(News)
+class NewsAdmin(SummernoteModelAdmin):
+    summer_note_fields = ('text_min', 'text')
+
+
+admin.site.register(News, NewsAdmin)
 admin.site.register(Category)
 admin.site.register(Tag)
