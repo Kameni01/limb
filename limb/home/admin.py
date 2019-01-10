57edf4d26ba21094fd0b3b9bfaa4dfef097c49fd
Kameni01 <kameni01@mail.ru>
2019-01-10 07:12:46 +0500

    Зарегистриваол модель в админке, добавил в модель дату создания комментария и пункт о возможности модерации(Пока функционал модерации не доступен)

--- limb/home/admin.py
+++ limb/home/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from home.models import News, Category, Tag
+from home.models import News, Category, Tag, Comments
 from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
@@ -8,5 +8,11 @@ class NewsAdmin(SummernoteModelAdmin):
 
 
 admin.site.register(News, NewsAdmin)
+
 admin.site.register(Category)
 admin.site.register(Tag)
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'new', 'created', 'moderation')
+
+admin.site.register(Comments, CommentAdmin)
--- limb/home/models.py
+++ limb/home/models.py
@@ -49,6 +49,8 @@ class Comments(models.Model):
     user = models.ForeignKey(User, verbose_name="Пользователь", on_delete=models.CASCADE)
     new = models.ForeignKey(News, verbose_name="Новость", on_delete=models.CASCADE)
     text = models.TextField("Комментарий")
+    created = models.DateTimeField("Дата добавления", auto_now_add=True, null=True)
+    moderation = models.BooleanField("Возможность модерации", default=False)
 
     class Meta:
         verbose_name = "Комментарий"
