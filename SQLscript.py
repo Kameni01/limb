4e78278fd5b0df938f8844a9b7cbdee9d5afb249
Kameni01 <kameni01@mail.ru>
2019-01-14 02:32:29 +0500

    Исправлен скрипт

--- SQLscript.py
+++ SQLscript.py
@@ -1,3 +1,7 @@
 import postgresql
 
 db = postgresql.open('pq://postgres:postgres@localhost:5432/limb')
+db.execute("CREATE USER admin WITH LOGIN SUPERUSER INHERIT CREATEDB CREATEROLE "
+"REPLICATION;GRANT postgres TO admin;CREATE DATABASE limb WITH OWNER = postgres ENCODING = 'UTF8'"
+"LC_COLLATE = 'Russian_Russia.1251' LC_CTYPE = 'Russian_Russia.1251' "
+"TABLESPACE = pg_default CONNECTION LIMIT = -1;")
